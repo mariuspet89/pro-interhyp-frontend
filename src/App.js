b82e42b580216052d52c4947cae9ad32695e67a7
ACCESA\doru.varga <ACCESA\doru.varga@accesa.eu>
2020-12-17 16:34:03 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+import Hello from "./components/hellotext";
+import React from "react";
+
+function App() {
+
+  return (
+
+      <div className="App">
+
+        <header className="App-header">
+
+          <p>Hello Super Juniors, I'm React</p>
+
+          <Hello/>
+
+        </header>
+
+      </div>
+  );
+}
+
+export default App;
