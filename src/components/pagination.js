208c16a4a44763be489df29e862f3c61e3fcaf3f
Alex Palacean <alex.palacean@accesa.eu>
2020-12-29 17:17:38 +0200

    pagination

--- src/components/pagination.js
+++ src/components/pagination.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Pagination = ({ usersPerPage, totalUsers, paginate }) => {
   const pageNumbers = [];
 
-  for (let i = 1; i <= Math.ceil(totalUsers / usersPerPage); i++) {
+  for (let i = 1; i <= totalUsers; i++) {
     pageNumbers.push(i);
   }
 
