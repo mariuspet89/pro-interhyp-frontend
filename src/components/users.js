236be216e13da6efc9f478e0fd408744c4dd73e2
MihaiSereanu <40597981+MihaiSereanu@users.noreply.github.com>
2021-01-11 17:18:34 +0200

    fixed API calls to work with state

--- src/components/users.js
+++ src/components/users.js
@@ -2,40 +2,54 @@ import React, { Component } from "react";
 import axios from "axios";
 import Pagination from "react-js-pagination";
 import User from "./User";
-import userList from '../styles/users.module.css'
+import userList from "../styles/users.module.css";
 
 class Users extends Component {
-    constructor(props) {
-        super(props);
-				this.state = {
-					users: [],
-					activePage: 1,
-					usersPerPage: 10,
-					currentUsers: 1,
-					sortFirstName: false,
-					sortLastName: false,
-					sortUsername: false,
-					sortJob: false,
-					sortBirthday: false,
-				};
- 		}
-		
+	constructor(props) {
+		super(props);
+		this.state = {
+			users: [],
+			activePage: 1,
+			usersPerPage: 10,
+			currentUsers: 1,
+			sortFirstName: false,
+			sortLastName: false,
+			sortUsername: false,
+			sortJob: false,
+			sortBirthday: false,
+		};
+	}
 
-  componentDidMount() {
-    axios.get("http://20.52.146.224:8080/users").then((response) => {
-      this.setState({ users: response.data });
-    });
+	componentDidMount() {
+		axios.get("http://20.52.146.224:8080/users").then((response) => {
+			this.setState({ users: response.data });
+		});
 	}
 
 	handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
-    this.setState({activePage: pageNumber});
+		console.log(`active page is ${pageNumber}`);
+		this.setState({ activePage: pageNumber });
+	}
+
+	deleteUser(id) {
+			if (window.confirm("Are you sure you want to delete?")) {
+				axios.delete( "http://20.52.146.224:8080/users/" + id)
+					.then((response) => {
+						this.setState({users: response.data})
+				})
+				// window.location.reload();
+			} else {
+				console.log("Delete, canceled");
+			}
 	}
 
 	render() {
 		const indexOfLastUser = this.state.activePage * this.state.usersPerPage;
-    const indexOfFirstUser = indexOfLastUser - this.state.usersPerPage;
-		const currentUsers = this.state.users.slice(indexOfFirstUser,indexOfLastUser);
+		const indexOfFirstUser = indexOfLastUser - this.state.usersPerPage;
+		const currentUsers = this.state.users.slice(
+			indexOfFirstUser,
+			indexOfLastUser
+		);
 
 		const handleSort = (e) => {
 			switch (e.target.parentElement.id) {
@@ -62,76 +76,76 @@ class Users extends Component {
 				default:
 					return;
 			}
-		}
+		};
 
-        return (
-					<>
-						<h2>Users</h2>
-						<div>
-							<table className={userList.tableContent}>
-								<thead>
-									<tr>
-										<th id='firstName'>
-											First Name
-											<span
-												className={userList.sortArrow}
-												onClick={(e) => handleSort(e)}>
-												{this.state.sortFirstName ? "▲" : "▼"}
-											</span>
-										</th>
-										<th id='lastName'>
-											Last Name
-											<span
-												className={userList.sortArrow}
-												onClick={(e) => handleSort(e)}>
-												{this.state.sortLastName ? "▲" : "▼"}
-											</span>
-										</th>
-										<th id='username'>
-											Username
-											<span
-												className={userList.sortArrow}
-												onClick={(e) => handleSort(e)}>
-												{this.state.sortUsername ? "▲" : "▼"}
-											</span>
-										</th>
-										<th id='job'>
-											Job
-											<span
-												className={userList.sortArrow}
-												onClick={(e) => handleSort(e)}>
-												{this.state.sortJob ? "▲" : "▼"}
-											</span>
-										</th>
-										<th id='birthday'>
-											Birthday
-											<span
-												className={userList.sortArrow}
-												onClick={(e) => handleSort(e)}>
-												{this.state.sortBirthday ? "▲" : "▼"}
-											</span>
-										</th>
-										<th>Details</th>
-										<th/>
-									</tr>
-								</thead>
-								<tbody>
-									{currentUsers.map((user) => (
-										<User key={user.id} user={user} />
-									))}
-								</tbody>
-							</table>
-						</div>
-						<Pagination
-							activePage={this.state.activePage}
-							itemsCountPerPage={this.state.usersPerPage}
-							totalItemsCount={this.state.users.length}
-							pageRangeDisplayed={5}
-							onChange={this.handlePageChange.bind(this)}
-						/>
-					</>
-				);
-		}
+		return (
+			<>
+				<h2>Users</h2>
+				<div>
+					<table className={userList.tableContent}>
+						<thead>
+							<tr>
+								<th id='firstName'>
+									First Name
+									<span
+										className={userList.sortArrow}
+										onClick={(e) => handleSort(e)}>
+										{this.state.sortFirstName ? "▲" : "▼"}
+									</span>
+								</th>
+								<th id='lastName'>
+									Last Name
+									<span
+										className={userList.sortArrow}
+										onClick={(e) => handleSort(e)}>
+										{this.state.sortLastName ? "▲" : "▼"}
+									</span>
+								</th>
+								<th id='username'>
+									Username
+									<span
+										className={userList.sortArrow}
+										onClick={(e) => handleSort(e)}>
+										{this.state.sortUsername ? "▲" : "▼"}
+									</span>
+								</th>
+								<th id='job'>
+									Job
+									<span
+										className={userList.sortArrow}
+										onClick={(e) => handleSort(e)}>
+										{this.state.sortJob ? "▲" : "▼"}
+									</span>
+								</th>
+								<th id='birthday'>
+									Birthday
+									<span
+										className={userList.sortArrow}
+										onClick={(e) => handleSort(e)}>
+										{this.state.sortBirthday ? "▲" : "▼"}
+									</span>
+								</th>
+								<th>Details</th>
+								<th />
+							</tr>
+						</thead>
+						<tbody>
+							{currentUsers.map((user) => (
+								<User key={user.id} user={user} deleteUser={this.deleteUser}/>
+							))}
+						</tbody>
+					</table>
+				</div>
+				<Pagination
+					activePage={this.state.activePage}
+					itemsCountPerPage={this.state.usersPerPage}
+					totalItemsCount={this.state.users.length}
+					pageRangeDisplayed={5}
+					onChange={this.handlePageChange.bind(this)}
+				/>
+			</>
+		);
 	}
+}
 
 export default Users;
