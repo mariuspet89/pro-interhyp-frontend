0e39f0bfd2aacdcdec133915774aa09c067a687f
ovidiu <ovidiu.barbu@accesa.eu>
2021-01-28 15:15:30 +0200

    routing css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,7 @@ import "../styles/navbar.css";
 import Logo from "../styles/accesa_white.jpg";
 import { useLocation } from "react-router-dom";
 import { SearchContext } from "./searchContext";
+import { NavLink, Link } from "react-router-dom";
 
 const Navbar = () => {
   const [searchValue, setSearchValue] = useContext(SearchContext);
@@ -10,6 +11,7 @@ const Navbar = () => {
     setSearchValue(e.target.value);
     console.log(e.target.value);
   };
+
   const location = useLocation();
   let search = null;
   if (location.pathname === "/")
@@ -21,15 +23,15 @@ const Navbar = () => {
   let navbar = (
     <div className="navbar">
       <div className="left">
-        <a href="/">
+        <Link to="/">
           <img src={Logo} alt="logo" />
-        </a>
-        <a href="/" className="links">
+        </Link>
+        <NavLink exact to="/" className="links" activeClassName="active">
           Users
-        </a>
-        <a href="/departments" className="links">
+        </NavLink>
+        <NavLink to="/departments" className="links" activeClassName="active">
           Departments
-        </a>
+        </NavLink>
       </div>
       {search}
     </div>
